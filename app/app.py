02eedffa5cf05168e8942df62706758199584503
Jeudess <joaoeudesjeudes@gmail.com>
2026-07-01 13:15:50 -0300

    Implementa funcionalidades da lista de tarefas

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from database import configure_database, db
 from models import Task
@@ -13,13 +13,48 @@ with app.app_context():
 
 @app.route("/")
 def index():
+    tarefas = Task.query.order_by(Task.id).all()
+    return render_template("index.html", tarefas=tarefas)
 
-    tarefas = Task.query.all()
 
-    return render_template(
-        "index.html",
-        tarefas=tarefas
-    )
+@app.route("/add", methods=["POST"])
+def add():
+
+    titulo = request.form["title"]
+
+    if titulo.strip():
+
+        nova = Task(title=titulo)
+
+        db.session.add(nova)
+
+        db.session.commit()
+
+    return redirect("/")
+
+
+@app.route("/complete/<int:id>")
+def complete(id):
+
+    tarefa = Task.query.get_or_404(id)
+
+    tarefa.completed = not tarefa.completed
+
+    db.session.commit()
+
+    return redirect("/")
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    tarefa = Task.query.get_or_404(id)
+
+    db.session.delete(tarefa)
+
+    db.session.commit()
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
